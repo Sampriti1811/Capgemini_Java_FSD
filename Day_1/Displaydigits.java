bc5a399ab85780bbbb6517fecf13306669f5e7b3
Sampriti1811 <101314948+Sampriti1811@users.noreply.github.com>
2022-03-10 12:46:26 +0530

    Add files via upload

---
+++ Day_1/Displaydigits.java
@@ -0,0 +1,19 @@
+package com.cg;
+import java.util.Scanner;
+public class Displaydigits{
+	
+
+	  public static void main(String[] args) {
+
+	    int i = 1, n = 5;
+
+	    // do...while loop from 1 to 5
+	    do {
+	      System.out.println(i);
+	      i++;
+	    } while(i <= n);
+	  }
+	}
+
+
+
