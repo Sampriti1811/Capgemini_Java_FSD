9b40e11e3b770f4b3ec4bdaeed2859ef666b706a
Sampriti1811 <101314948+Sampriti1811@users.noreply.github.com>
2022-03-10 12:53:12 +0530

    Add files via upload

---
+++ Day_2/Arrays.java
@@ -0,0 +1,13 @@
+package comm.cg;
+
+public class Arrays {
+	public static void main(String[] args)
+	{
+
+		int[][] arr = { { 1, 2 }, { 3, 4 } };
+
+		System.out.println("arr[0][0] = " + arr[0][0]);
+	}
+
+
+}
