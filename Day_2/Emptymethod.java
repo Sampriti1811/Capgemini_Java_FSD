b44d15a3c1136fbb56c9975b4681fafe6f5bf41a
Sampriti1811 <101314948+Sampriti1811@users.noreply.github.com>
2022-03-10 12:54:51 +0530

    Add files via upload

---
+++ Day_2/Emptymethod.java
@@ -0,0 +1,15 @@
+package comm.cg;
+
+public class Emptymethod {
+	public static void main(String[] args) {
+
+	    String str1 = "Java Programming";
+	    String str2 = "";
+
+	    System.out.println(str1.isEmpty()); // false
+	    System.out.println(str2.isEmpty()); // true
+
+	  }
+
+
+}
