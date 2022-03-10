cfb800f2d10198ce42a784997b73408474243166
Sampriti1811 <101314948+Sampriti1811@users.noreply.github.com>
2022-03-10 12:47:06 +0530

    Add files via upload

---
+++ Day_1/Forloop.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+public class Forloop {
+	public static void main(String args[])
+		{
+			int x;
+			for (x = 0; x < 4; x++)
+			{
+				System.out.println(x);
+			}
+
+		System.out.println(x);
+		}
+	}
