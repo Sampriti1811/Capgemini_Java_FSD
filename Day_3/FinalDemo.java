635fc04edab52afc4d302eb5c44b306812896fc9
Sampriti1811 <101314948+Sampriti1811@users.noreply.github.com>
2022-03-10 13:01:37 +0530

    Add files via upload

---
+++ Day_3/FinalDemo.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class FinalDemo {
+	final int DAYS_IN_WEEK;
+	FinalDemo(){
+		DAYS_IN_WEEK=7;
+	}
+	public static void main(String[] args) {
+		FinalDemo finalDemo=new FinalDemo();
+	}
+
+}
