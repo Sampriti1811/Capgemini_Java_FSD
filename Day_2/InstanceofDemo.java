d2503765199d02e16aebdee5e324231e9c4a43c8
Sampriti1811 <101314948+Sampriti1811@users.noreply.github.com>
2022-03-10 12:58:35 +0530

    Add files via upload

---
+++ Day_2/InstanceofDemo.java
@@ -0,0 +1,13 @@
+package comm.cg;
+class A{}
+class B extends A{}
+public class InstanceofDemo
+{
+	public static void main(String[] args)
+	{
+		B objOfClassB = new B();
+		
+		System.out.println("objOfClassB is an instance of B: " + (objOfClassB instanceof B));
+		System.out.println("objOfClassB is an instance of the parent class A: " + (objOfClassB instanceof A));	
+	}
+}
